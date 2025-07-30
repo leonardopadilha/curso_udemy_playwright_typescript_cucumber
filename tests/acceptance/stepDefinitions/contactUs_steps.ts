9972e9ec9d1162513efd85cb9e8b8f48009bdd37
Leonardo Padilha <leonardopadilha28@gmail.com>
2025-07-30 05:56:23 -0300

    Adicionando fakerjs aos cenários

--- tests/acceptance/stepDefinitions/contactUs_steps.ts
+++ tests/acceptance/stepDefinitions/contactUs_steps.ts
@@ -2,6 +2,13 @@
 import { Given, When, Then } from "@cucumber/cucumber";
 import { pageFixture } from "./hooks/browserContextFixture";
 import { expect } from "@playwright/test";
+import { faker } from '@faker-js/faker';
+
+const user = {
+  firstName: faker.person.firstName(),
+  lastName: faker.person.lastName(),
+  email: faker.internet.email(),
+}
 
 When('I type a first name', async () =>{
   await pageFixture.page.locator('[placeholder="First Name"]').fill("John")
@@ -50,4 +57,17 @@ When('I enter a specific email address {string}', async (email: string) => {
 
 When('I type a specific text {string} and a number {int} within the comment input field', async (message: string, numberComment: number) => {
   await pageFixture.page.getByPlaceholder('Comments').fill(`${message} ${numberComment}`)
+});
+
+// Random Data - Faker
+When('I type a random first name', async () => {
+  await pageFixture.page.locator('[placeholder="First Name"]').fill(user.firstName)
+});
+
+When('I type a random last name', async () => {
+  await pageFixture.page.locator('[placeholder="Last Name"]').fill(user.lastName)
+});
+
+When('I enter a random email address', async () => {
+  await pageFixture.page.locator('//input[contains(@placeholder, "Email")]').fill(user.email)
 });
