7fb7e6e780b12100876c8a3a0fb0e7bc9d638677
Leonardo Padilha <leonardopadilha28@gmail.com>
2025-08-11 12:39:08 -0300

    Iniciando a criação de page objects

---
+++ tests/acceptance/page-objects/base/BasePage.ts
@@ -0,0 +1,32 @@
+import { Page, Locator } from "@playwright/test"
+import { pageFixture } from "../../stepDefinitions/hooks/browserContextFixture"
+
+export class BasePage {
+  get page(): Page {
+    return pageFixture.page
+  }
+
+  // Promise<void> in TypeScript when you're defining an async function that doesn't explicitly return a value
+  public async navigate(url: string): Promise<void> {
+    await this.page.goto(url)
+  }
+
+  public async waitAndClickByRole(role: string, name: string): Promise<void> {
+    const element = await this.page.getByRole(role as any, { name: name })
+    await element.click()
+  }
+
+  // Wait for the locator to be visible and then click on it
+  public async waitAndClick(locator: Locator): Promise<void> {
+    await locator.isVisible()
+    await locator.click()
+  }
+
+  // Wait for the selector to be visible and then click on it
+  public async waitAndClickSelector(selector: string): Promise<void> {
+    await this.page.waitForSelector(selector)
+    await this.page.click(selector)
+  }
+
+
+}
